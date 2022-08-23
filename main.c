b026b74ee327699d6efef9c136c4a6868b766a8d
ytasan <yasintasan@hotmail.com>
2022-08-23 11:50:59 +0300

    sum all array elements with order - brute force

--- main.c
+++ main.c
@@ -3,6 +3,8 @@
  */
 int* twoSum(int* nums, int numsSize, int target, int* returnSize){    
     
+    unsigned int sum = 0;
+    
     for (unsigned int i = 0; i<numsSize; i++){
         printf("numbers = %d\n", nums[i]);
     }
@@ -10,6 +12,14 @@ int* twoSum(int* nums, int numsSize, int target, int* returnSize){
     printf("numsSize = %d\n", numsSize);
     printf("target = %d\n", target);
     printf("returnSize = %d\n", *returnSize);
+    
+    for (unsigned int i = 0; i < numsSize; i++){
+        for (unsigned int j = i + 1; j < numsSize; j++){
+            printf("i = %d j = %d\n", i, j);
+            sum = nums[i] + nums[j];             
+            printf("sum = %d\n", sum);
+        }        
+    }
     
     return returnSize;
 }

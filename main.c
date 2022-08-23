f2bd498a52f9bd276fd233d79426d77829ab26b6
ytasan <yasintasan@hotmail.com>
2022-08-23 12:15:01 +0300

    result OK

--- main.c
+++ main.c
@@ -3,23 +3,34 @@
  */
 int* twoSum(int* nums, int numsSize, int target, int* returnSize){    
     
-    unsigned int sum = 0;
+    unsigned int sum = 0;    
+    *returnSize = 2;
+    int* asd = (int*)malloc(*returnSize * sizeof(int));
+        
     
-    for (unsigned int i = 0; i<numsSize; i++){
-        printf("numbers = %d\n", nums[i]);
-    }
-    printf("----\n");
-    printf("numsSize = %d\n", numsSize);
-    printf("target = %d\n", target);
-    printf("returnSize = %d\n", *returnSize);
+    // for (unsigned int i = 0; i<numsSize; i++){
+    //     printf("numbers = %d\n", nums[i]);
+    // }
+    // printf("----\n");
+    // printf("numsSize = %d\n", numsSize);
+    // printf("target = %d\n", target);
+    // printf("returnSize = %d\n", *returnSize);
     
-    for (unsigned int i = 0; i < numsSize; i++){
-        for (unsigned int j = i + 1; j < numsSize; j++){
-            printf("i = %d j = %d\n", i, j);
+    for (int i = 0; i < numsSize; i++){
+        for (int j = i + 1; j < numsSize; j++){
+            // printf("i = %d, j = %d\n", i, j);
             sum = nums[i] + nums[j];             
-            printf("sum = %d\n", sum);
+            // printf("sum = %d\n", sum);
+            if (sum == target){
+                asd[0] = i;
+                asd[1] = j;
+                goto targetOK;
+            }
         }        
     }
     
-    return returnSize;
+targetOK:
+    // printf("sum = %d\n", sum);
+    // printf("[%d,%d]", result[0], result[1]);
+    return asd;
 }
